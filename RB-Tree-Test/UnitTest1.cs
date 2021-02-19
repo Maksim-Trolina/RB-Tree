f79a6b6e95749484baed22701f04c2e4dd91defb
Maksim <maks.panasenko01@bk.ru>
2021-02-19 20:53:35 +0300

    .

---
+++ RB-Tree-Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace RB_Tree_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
